563e0b28dc36325d3b53efde679baad652f1f98b
Valdeir Gabriel da Silva <valdeirgabriels@gmail.com>
2022-04-01 20:28:28 -0300

    Validações de usuários

--- backend/db/conn.js
+++ backend/db/conn.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 async function main() {
-  await mongoose.connet('mongodb://localhost:27017/getapet');
+  await mongoose.connect('mongodb://localhost:27017/getapet');
   console.log('Conectou ao Mongoose!');
 }
 
