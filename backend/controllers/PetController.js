afe7f097fe4bd6b60402be15c7c80b99275e363f
Valdeir Gabriel da Silva <valdeirgabriels@gmail.com>
2022-04-10 16:01:47 -0300

    Agendando visita de adoção

--- backend/controllers/PetController.js
+++ backend/controllers/PetController.js
@@ -230,4 +230,46 @@ module.exports = class PetController {
 
     res.status(200).json({ message: 'Pet atualizado com sucesso!' });
   }
+
+  static async schedule(req, res) {
+    const { id } = req.params;
+
+    // check if pet exists
+    const pet = await Pet.findById(id);
+
+    if (!pet) {
+      res.status(404).json({ message: 'Pet não encontrado!' });
+      return;
+    }
+
+    // check if user registered the pet
+    const token = getToken(req);
+    const user = await getUserByToken(token);
+
+    if (pet.user._id.equals(user._id)) {
+      res.status(422).json({ message: 'Você não pode agendar uma visita para o seu próprio Pet!' });
+      return;
+    }
+
+    //check if user has already scheduled a visit
+    if (pet.adopter) {
+      if (pet.adopter._id.equals(user._id)) {
+        res.status(422).json({ message: 'Você já agendou uma visita para este Pet!' });
+        return;
+      }
+    }
+
+    // add user to pet
+    pet.adopter = {
+      _id: user._id,
+      name: user.name,
+      image: user.image
+    };
+
+    await Pet.findByIdAndUpdate(id, pet);
+
+    res.status(200).json({
+      message: `A visita foi agendada com sucesso, entre em contato com ${pet.user.name} pelo telefone ${pet.user.phone}`
+    });
+  }
 }
