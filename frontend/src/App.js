1adfe28c8c48d0923c6a8dd0d32c1033ef2397e7
Valdeir Gabriel da Silva <valdeirgabriels@gmail.com>
2022-04-11 22:24:26 -0300

    Criando componente de mensagem

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,6 +4,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Navbar from './components/layout/Navbar';
 import Footer from './components/layout/Footer';
 import Container from './components/layout/Container';
+import Message from './components/layout/Message';
 
 // pages
 import Login from './components/pages/Auth/Login';
@@ -18,6 +19,7 @@ function App() {
     <Router>
       <UserProvider>
         <Navbar />
+        <Message />
         <Container>
           <Routes>
             <Route path='/login' element={<Login />} />
