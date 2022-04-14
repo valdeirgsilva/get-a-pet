40248c8005ff8d00d3ad09b486041158c6b4f16a
Valdeir Gabriel da Silva <valdeirgabriels@gmail.com>
2022-04-14 11:48:43 -0300

    Atualização do usuário

--- frontend/src/components/pages/User/Profile.js
+++ frontend/src/components/pages/User/Profile.js
@@ -7,9 +7,12 @@ import formStyles from '../../form/Form.module.css';
 
 import Input from '../../form/Input';
 
+import useFlashMessage from '../../../hooks/useFlashMessage';
+
 function Profile() {
   const [user, setUser] = useState({});
   const [token] = useState(localStorage.getItem('token') || '');
+  const { setFlashMessage } = useFlashMessage();
 
   useEffect(() => {
     api.get('/users/checkuser', {
@@ -21,9 +24,37 @@ function Profile() {
     });
   }, [token]);
 
-  function onFileChange(e) { }
+  function onFileChange(e) {
+    setUser({ ...user, [e.target.name]: e.target.files[0] });
+  }
 
-  function handleChange(e) { }
+  function handleChange(e) {
+    setUser({ ...user, [e.target.name]: e.target.value });
+  }
+
+  async function handleSubmit(e) {
+    e.preventDefault();
+
+    let msgType = 'success';
+
+    const formData = new FormData();
+
+    await Object.keys(user).forEach(key => formData.append(key, user[key]));
+
+    const data = await api.patch(`/users/edit/${user._id}`, formData, {
+      headers: {
+        Authorization: `Bearer ${JSON.parse(token)}`,
+        'Content-Type': 'multipart/form-data'
+      }
+    }).then(response => {
+      return response.data;
+    }).catch(err => {
+      msgType = 'error';
+      return err.response.data;
+    });
+
+    setFlashMessage(data.message, msgType);
+  }
 
   return (
     <section>
@@ -31,7 +62,7 @@ function Profile() {
         <h1>Perfil</h1>
         <p>Preview Images</p>
       </div>
-      <form className={formStyles.form_container}>
+      <form onSubmit={handleSubmit} className={formStyles.form_container}>
         <Input
           text="Imagem"
           type="file"
