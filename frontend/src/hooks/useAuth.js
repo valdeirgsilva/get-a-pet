f80bdd8c08c13566bb15d6147346663e6c3fea5f
Valdeir Gabriel da Silva <valdeirgabriels@gmail.com>
2022-04-14 10:27:48 -0300

    Função de login

--- frontend/src/hooks/useAuth.js
+++ frontend/src/hooks/useAuth.js
@@ -36,6 +36,24 @@ export default function useAuth() {
     setFlashMessage(msgText, msgType);
   }
 
+  async function login(user) {
+    let msgText = 'Login realizado com sucesso!';
+    let msgType = 'success';
+
+    try {
+      const data = await api.post('/users/login', user).then(response => {
+        return response.data;
+      });
+
+      await authUser(data);
+    } catch(error) {
+      msgText = error.response.data.message;
+      msgType = 'error';
+    }
+
+    setFlashMessage(msgText, msgType);
+  }
+
   async function authUser(data) {
     setAuthenticated(true);
     localStorage.setItem('token', JSON.stringify(data.token));
@@ -55,5 +73,5 @@ export default function useAuth() {
     setFlashMessage(msgText, msgType);
   }
 
-  return { authenticated, register, logout };
+  return { authenticated, register, logout, login };
 }
