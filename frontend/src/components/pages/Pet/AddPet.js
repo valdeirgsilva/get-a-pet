c11c59612d0a74cd5fc8eca2a4cdd3b05dddc38e
Valdeir Gabriel da Silva <valdeirgabriels@gmail.com>
2022-04-20 18:44:46 -0300

    Página de cadastro do Pet

---
+++ frontend/src/components/pages/Pet/AddPet.js
@@ -0,0 +1,23 @@
+import api from '../../../utils/api';
+
+import styles from './AddPet.module.css';
+
+import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+
+// hooks
+import useFlashMessage from '../../../hooks/useFlashMessage';
+
+function AddPet() {
+  return (
+    <section className={styles.addpet_header}>
+      <div>
+        <h1>Cadastre um Pet</h1>
+        <p>Depois ele ficará disponível para adoção</p>
+      </div>
+      <p>formulário</p>
+    </section>
+  );
+}
+
+export default AddPet;
