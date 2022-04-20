0fd09eb6ddf86ee1cf294ab6f648a56ef8d28217
Valdeir Gabriel da Silva <valdeirgabriels@gmail.com>
2022-04-20 18:30:06 -0300

    Criando página de lista de pets

---
+++ frontend/src/components/pages/Pet/MyPets.js
@@ -0,0 +1,22 @@
+import { useState, useEffect } from 'react';
+
+import { Link } from 'react-router-dom';
+
+function MyPets() {
+  const [pets, setPets] = useState([]);
+
+  return (
+    <section>
+      <div>
+        <h1>My Pets</h1>
+        <Link to='/pet/add'>Cadastrar Pet</Link>
+      </div>
+      <div>
+        {pets.length > 0 && <p>Meus Pets cadastrados</p>}
+        {pets.length === 0 && <p>Não há Pets cadastrados</p>}
+      </div>
+    </section>
+  );
+}
+
+export default MyPets;
