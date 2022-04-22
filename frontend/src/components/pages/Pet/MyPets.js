6ff9b7a3b310570545e3f90fd3319ed7fe2e7499
Valdeir Gabriel da Silva <valdeirgabriels@gmail.com>
2022-04-22 17:49:58 -0300

    Conclusão da adoção

--- frontend/src/components/pages/Pet/MyPets.js
+++ frontend/src/components/pages/Pet/MyPets.js
@@ -49,6 +49,25 @@ function MyPets() {
     setFlashMessage(data.message, msgType);
   }
 
+  async function concludeAdoption(id) {
+    let msgType = 'success';
+
+    const data = await api.patch(`/pets/conclude/${id}`, {
+      headers: {
+        Authorization: `Bearer ${JSON.parse(token)}`
+      }
+    })
+    .then(response => {
+      return response.data;
+    })
+    .catch(err => {
+      msgType = 'error';
+      return err.response.data;
+    });
+
+    setFlashMessage(data.message, msgType);
+  }
+
   return (
     <section>
       <div className={styles.petlist_header}>
@@ -69,7 +88,12 @@ function MyPets() {
                 {pet.available ?
                   (<>
                     {pet.adopter && (
-                      <button className={styles.conclude_btn}>Concluir adoção</button>
+                      <button
+                        className={styles.conclude_btn}
+                        onClick={() => concludeAdoption(pet._id)}
+                      >
+                        Concluir adoção
+                      </button>
                     )}
                     <Link to={`/pet/edit/${pet._id}`}>Editar</Link>
                     <button
