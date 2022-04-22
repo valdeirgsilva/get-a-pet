c9101fc3d41e49a58e1de33478592def9b3c2c2a
Valdeir Gabriel da Silva <valdeirgabriels@gmail.com>
2022-04-22 10:53:43 -0300

    Atualizando o pet

--- frontend/src/components/pages/Pet/EditPet.js
+++ frontend/src/components/pages/Pet/EditPet.js
@@ -5,11 +5,10 @@ import { useParams } from 'react-router-dom';
 
 import styles from './AddPet.module.css';
 
-import PerForm from '../../form/PetForm';
+import PetForm from '../../form/PetForm';
 
 // hooks
 import useFlashMessage from '../../../hooks/useFlashMessage';
-import PetForm from '../../form/PetForm';
 
 function EditPet() {
   const [pet, setPet] = useState({});
@@ -25,9 +24,39 @@ function EditPet() {
       .then(response => {
         setPet(response.data.pet);
       });
-  }, [token, id]);
+    }, [token, id]);
 
-  async function updatePet(pet) {}
+  async function updatePet(pet) {
+    let msgType = 'success';
+
+    const formData = new FormData();
+
+    await Object.keys(pet).forEach(key => {
+      if (key === 'images') {
+        for(let i = 0; i < pet[key].length; i++) {
+          formData.append('images', pet[key][i]);
+        }
+      } else {
+        formData.append(key, pet[key]);
+      }
+    });
+
+    const data = await api.patch(`pets/${pet._id}`, formData, {
+      headers: {
+        Authorization: `Bearer ${JSON.parse(token)}`,
+        'Content-Type': 'multiárt/form-data'
+      }
+    })
+    .then(response => {
+      return response.data;
+    })
+    .catch(err => {
+      msgType = 'error';
+      return err.response.data;
+    });
+
+    setFlashMessage(data.message, msgType);
+  }
 
   return (
     <section>
